b75f788f56eafd883eda25344803170b992d6e24
Kingsley Ugochukwu <102130041+k1ngsley2@users.noreply.github.com>
2023-06-30 14:44:03 +0200

    Add files via upload

---
+++ GenerationCODE.py
@@ -0,0 +1,32 @@
+print()
+print(" This code will telll you which generation you belong to.")
+print()
+print("Lets get started!")
+print()
+Yearofbirth = int(input("Which year were you born?: "))
+if Yearofbirth >= 1925 and Yearofbirth <= 1947:
+  print()
+  print("You are from the Traditionalist Generation")
+  print()
+elif Yearofbirth >= 1947 and Yearofbirth <= 1964:
+  print()
+  print("You are from the Baby Boomers Generation")
+  print()
+elif Yearofbirth >= 1965 and Yearofbirth <= 1982:
+  print()
+  print("You were born from the Baby Boomers Generation")
+  print()
+elif Yearofbirth >= 1982 and Yearofbirth <= 1995:
+  print()
+  print("You are from Gen X Generation")
+  print()
+elif Yearofbirth >= 1996 and Yearofbirth <= 2015:
+  print()
+  print("You were born from Gen Z Generation")
+  print()
+else:
+  print()
+  print("You are too young to play this game!!!")
+  print()
+  
+  
